b18b66b170368b1f42be46ffc245ac50ce52d39c
Kaitlyn Martocci <kdm342@drexel.edu>
2025-05-06 13:04:56 -0400

    I started everything over to format my files properly

---
+++ encore-react/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="frame">
+        <h1>Welcome to Encore</h1>
+        <p>Encore, because the crowd always wants one more</p>
+        <button>Login With Spotify</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
