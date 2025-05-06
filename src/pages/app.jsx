57a0dd19873e325721706db251e36cec69008d1a
Kaitlyn Martocci <kdm342@drexel.edu>
2025-05-06 10:55:28 -0400

    changing files around

---
+++ src/pages/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+      <h1>🎧 Welcome to Encore</h1>
+      <p>Build smart DJ setlists based on events and song vibes.</p>
+    </div>
+  );
+}
